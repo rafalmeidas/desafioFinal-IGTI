77d3abe6d871e8c7bff989b5b48f9feb9540fc0e
Rafael Almeida <supremotop10@gmail.com>
2021-07-27 23:20:04 -0300

    ADD lógica de agrupar times, pontos, vitórias, empates, derrotas do ano selecionado

--- src/api/api.js
+++ src/api/api.js
@@ -5,35 +5,122 @@ const api = axios.create({
 });
 
 export default function getClassificacaoPorAnoEndpoint() {
-  let newArray = [{}];
   return api.get('/2003').then(res => {
-    res.data.map(d => {
-      let partida = d.partidas;
+    let novoLista = [];
+
+    let m = new Map();
+    let temporada = res.data;
+
+    for (let partidas of temporada) {
+      let partida = partidas.partidas;
       for (let p of partida) {
-        let index = newArray.findIndex(val => val.visitante === p.visitante);
-        //console.log(index);
-        if (index > 0) {
-          //console.log(p.visitante + ' tem');
+        if (m.has(p.mandante)) {
+          //No item indexado por prod.nome no array novoLista incrementa o preço registrado com o novo preço.
+          novoLista[m.get(p.mandante)].vitorias =
+            p.pontuacao_geral_mandante.total_vitorias >
+            novoLista[m.get(p.mandante)].vitorias
+              ? p.pontuacao_geral_mandante.total_vitorias
+              : novoLista[m.get(p.mandante)].vitorias;
+
+          novoLista[m.get(p.mandante)].empates =
+            p.pontuacao_geral_mandante.total_empates >
+            novoLista[m.get(p.mandante)].empates
+              ? p.pontuacao_geral_mandante.total_empates
+              : novoLista[m.get(p.mandante)].empates;
+
+          novoLista[m.get(p.mandante)].derrotas =
+            p.pontuacao_geral_mandante.total_derrotas >
+            novoLista[m.get(p.mandante)].derrotas
+              ? p.pontuacao_geral_mandante.total_derrotas
+              : novoLista[m.get(p.mandante)].derrotas;
+
+          novoLista[m.get(p.mandante)].pontos =
+            p.pontuacao_geral_mandante.total_pontos >
+            novoLista[m.get(p.mandante)].pontos
+              ? p.pontuacao_geral_mandante.total_pontos
+              : novoLista[m.get(p.mandante)].pontos;
+
+          novoLista[m.get(p.mandante)].gols_pro =
+            p.pontuacao_geral_mandante.total_gols_marcados >
+            novoLista[m.get(p.mandante)].gols_pro
+              ? p.pontuacao_geral_mandante.total_gols_marcados
+              : novoLista[m.get(p.mandante)].gols_pro;
+
+          novoLista[m.get(p.mandante)].gols_contra =
+            p.pontuacao_geral_mandante.total_gols_sofridos >
+            novoLista[m.get(p.mandante)].gols_contra
+              ? p.pontuacao_geral_mandante.total_gols_sofridos
+              : novoLista[m.get(p.mandante)].gols_contra;
         } else {
-          newArray.push({ visitante: p.visitante });
-          //console.log(p.visitante + ' não tem');
+          //Cria um índice nomeado pelo valor de prod.nome apontando para o mais novo elemento do array novoLista
+          m.set(
+            p.mandante,
+            novoLista.push({
+              mandante: p.mandante,
+              vitorias: p.pontuacao_geral_mandante.total_vitorias,
+              empates: p.pontuacao_geral_mandante.total_empates,
+              derrotas: p.pontuacao_geral_mandante.total_derrotas,
+              pontos: p.pontuacao_geral_mandante.total_pontos,
+              gols_pro: p.pontuacao_geral_mandante.total_gols_marcados,
+              gols_contra: p.pontuacao_geral_mandante.total_gols_sofridos,
+            }) - 1
+          );
+        }
+
+        if (m.has(p.visitante)) {
+          //No item indexado por prod.nome no array novoLista incrementa o preço registrado com o novo preço.
+          novoLista[m.get(p.visitante)].vitorias =
+            p.pontuacao_geral_visitante.total_vitorias >
+            novoLista[m.get(p.visitante)].vitorias
+              ? p.pontuacao_geral_visitante.total_vitorias
+              : novoLista[m.get(p.visitante)].vitorias;
+
+          novoLista[m.get(p.visitante)].empates =
+            p.pontuacao_geral_visitante.total_empates >
+            novoLista[m.get(p.visitante)].empates
+              ? p.pontuacao_geral_visitante.total_empates
+              : novoLista[m.get(p.visitante)].empates;
+
+          novoLista[m.get(p.visitante)].derrotas =
+            p.pontuacao_geral_visitante.total_derrotas >
+            novoLista[m.get(p.visitante)].derrotas
+              ? p.pontuacao_geral_visitante.total_derrotas
+              : novoLista[m.get(p.visitante)].derrotas;
+
+          novoLista[m.get(p.visitante)].pontos =
+            p.pontuacao_geral_visitante.total_pontos >
+            novoLista[m.get(p.visitante)].pontos
+              ? p.pontuacao_geral_visitante.total_pontos
+              : novoLista[m.get(p.visitante)].pontos;
+
+          novoLista[m.get(p.visitante)].gols_pro =
+            p.pontuacao_geral_visitante.total_gols_marcados >
+            novoLista[m.get(p.visitante)].gols_pro
+              ? p.pontuacao_geral_visitante.total_gols_marcados
+              : novoLista[m.get(p.visitante)].gols_pro;
+
+          novoLista[m.get(p.visitante)].gols_contra =
+            p.pontuacao_geral_visitante.total_gols_sofridos >
+            novoLista[m.get(p.visitante)].gols_contra
+              ? p.pontuacao_geral_visitante.total_gols_sofridos
+              : novoLista[m.get(p.visitante)].gols_contra;
+        } else {
+          //Cria um índice nomeado pelo valor de prod.nome apontando para o mais novo elemento do array novoLista
+          m.set(
+            p.visitante,
+            novoLista.push({
+              visitante: p.visitante,
+              vitorias: p.pontuacao_geral_visitante.total_vitorias,
+              empates: p.pontuacao_geral_visitante.total_empates,
+              derrotas: p.pontuacao_geral_visitante.total_derrotas,
+              pontos: p.pontuacao_geral_visitante.total_pontos,
+              gols_pro: p.pontuacao_geral_visitante.total_gols_marcados,
+              gols_contra: p.pontuacao_geral_visitante.total_gols_sofridos,
+            }) - 1
+          );
         }
       }
-    });
-    return newArray;
-    console.log(newArray);
+    }
+    return novoLista;
   });
 }
-
-/**
- partida.map(p => {
-        //console.log(p.visitante);
-        if (newArray.includes(g => g.visitante === 'Vasco')) {
-          console.log('tem');
-          newArray.push({ visitante: p.visitante, total_gol: 5 });
-        } else {
-          console.log('não tem');
-          newArray.push({ visitante: p.visitante });
-        }
-      });
- */
